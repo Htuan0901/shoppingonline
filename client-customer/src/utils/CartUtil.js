a7d3b026942542c9a32c516930adcb66d9350aec
Htuan0901 <tuan.2374802010542@vanlanguni.vn>
2026-04-03 07:53:06 -0400

    fix submodule error

---
+++ client-customer/src/utils/CartUtil.js
@@ -0,0 +1,12 @@
+const CartUtil = {
+  getTotal(mycart) {
+    let total = 0;
+    for (const item of mycart) {
+      total += item.product.price * item.quantity;
+    }
+    return total;
+  },
+};
+
+export default CartUtil;
+
---
+++ server/utils/MyConstants.js
@@ -0,0 +1,12 @@
+const MyConstants = {
+  DB_SERVER: 'shoponline.anuyehg.mongodb.net',
+  DB_USER: 'Hoangtuan0901',
+  DB_PASS: 'Gonlitshin00',
+  DB_DATABASE: 'shoppingonline',
+  EMAIL_USER: 'troyesivan077@gmail.com', // Microsoft mail service
+  EMAIL_PASS: 'tsyo jcqr ggjc jwpv',
+  JWT_SECRET: 'shoppingonline_secret_key',
+  // e.g. 600000 ms = 10 minutes
+  JWT_EXPIRES: 86400000,
+};
+module.exports = MyConstants;
